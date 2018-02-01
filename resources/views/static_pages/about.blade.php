797117d27010f6e8fb70d262f535d21a1b415f44
vagrant <vagrant@homestead>
2018-02-01 02:42:37 +0000

    Finish STATIC PAGES

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Sample app</title>
+</head>
+
+<body>
+  <h1>关于</h1>
+</body>
+
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Sample app</title>
+</head>
+
+<body>
+  <h1>帮助页</h1>
+</body>
+
+</html>
