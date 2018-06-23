b34d263a48fe8252891ad770e89394ef1531eca6
JY <614622202@qq.com>
2018-06-23 18:43:33 +0800

    0623 user-crud01

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,4 +38,35 @@ class UserController extends Controller
         session()->flash('success','恭喜您，注册成功！');
         return redirect(route('user.show',[$user]));
     }
+
+    /**
+     * 编辑个人资料初始化
+     */
+    public function edit(User $user){
+
+        return view('users.edit',compact('user'));
+    }
+
+    /**
+     * 修改资料
+     */
+    public function update(User $user,Request $request){
+
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6:'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+
+        if ($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user ->update($data);
+        session()->flash('success','资料修改成功!');
+
+        return redirect()->route('user.show',$user->id);
+    }
 }
